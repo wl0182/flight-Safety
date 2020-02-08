e50c64696c42f59d0b1497e576d2f7a2484e2473
Wassim Lagnaoui <lagnaouiw@gmail.com>
2020-02-08 17:52:24 -0600

    Initial Commit

---
+++ At Training/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  At Training
+//
+//  Created by WASSIM LAGNAOUI on 2/8/20.
+//  Copyright © 2020 WASSIM LAGNAOUI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

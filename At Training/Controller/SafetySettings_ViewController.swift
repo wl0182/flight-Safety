d43d8f309efcddc8780759987049c7a1dc71b8ac
Wassim Lagnaoui <lagnaouiw@gmail.com>
2020-02-22 22:00:13 -0600

    added all view controllers frames

---
+++ At Training/Controller/SafetySettings_ViewController.swift
@@ -0,0 +1,31 @@
+
+//
+//  SafetySettings_ViewController.swift
+//  At Training
+//
+//  Created by WASSIM LAGNAOUI on 2/22/20.
+//  Copyright © 2020 WASSIM LAGNAOUI. All rights reserved.
+//
+
+import UIKit
+
+class SafetySettings_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

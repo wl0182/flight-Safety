01b310740b5ac3b0436d1ea6864e840f8046a849
Wassim Lagnaoui <lagnaouiw@gmail.com>
2020-04-14 14:09:20 -0500

    added pickerDelegate to the Safety settings VC

--- At Training/Controller/SafetySettings_ViewController.swift
+++ At Training/Controller/SafetySettings_ViewController.swift
@@ -9,23 +9,94 @@
 
 import UIKit
 
-class SafetySettings_ViewController: UIViewController {
+class SafetySettings_ViewController: UIViewController , UIPickerViewDelegate , UIPickerViewDataSource {
+    
+    @IBOutlet weak var picker: UIPickerView!
+    var minRoll: Int = 0
+    var minPitch: Int = 0
+    var maxRoll: Int = 0
+    var maxPitch: Int = 0
+    
+    
+    // Safety Settings data values
+    let pickerData = [["-30,+30","-20,+20","-10,+10"],
+                      ["-30,+30","-20,+20","-10,+10"],
+                      ["-30,+30","-20,+20","-10,+10"],
+                      ["-30,+30","-20,+20","-10,+10"]]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        
         // Do any additional setup after loading the view.
+        picker.delegate = self
+        picker.dataSource = self
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 4
     }
-    */
+    
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 3
+        
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        
+        //        return String(pickerData[component][row])
+        return pickerData[component][row]
+        
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if component == 0 {
+            if row == 0 {
+                minRoll = -30
+                maxRoll = 30
+            }
+            
+            if row == 1 {
+                minRoll = -20
+                maxRoll = 20
+            }
+            
+            if row == 2 {
+                minRoll = -10
+                maxRoll = 10
+            }
+        }
+        
+        if component == 1 {
+            if row == 0 {
+                minPitch = -30
+                maxPitch = 30
+                
+            }
+            
+            if row == 1 {
+                minPitch = -20
+                maxPitch = 20
+                
+            }
+            
+            if row == 2 {
+                minPitch = -10
+                maxPitch = 10
+            }
+        }
+        
+       
+        
+        
+    }
+    
+   
+    
+
 
 }
